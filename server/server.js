91f68ee856b00781557b2062e311e0224b6492e6
Ward Cunningham <ward@c2.com>
2017-05-13 15:37:38 -0700

    report in csv format, 0.2.3

--- server/server.js
+++ server/server.js
@@ -5,25 +5,57 @@
   var glob = require('glob')
   var async = require('async')
   var jsonfile = require('jsonfile')
+  var csvWriter = require('csv-write-stream')
 
   function startServer(params) {
     var app = params.app
     var argv = params.argv
-    app.get('/plugin/fivestar/ratings.json', ratings)
+    app.get('/plugin/fivestar/ratings.json', ratingsJson)
+    app.get('/plugin/fivestar/ratings.csv', ratingsCsv)
 
-    function ratings(req, res) {
+    function ratingsCsv(req, res) {
+      collectRatings(collectDone)
+      function collectDone(err, report) {
+        var heading = {}
+        for (var page in report) {
+          for (var rating in report[page]) {
+            heading[rating] = true
+          }
+        }
+        var headers = Object.keys(heading)
+        var writer = csvWriter({headers: ['Title'].concat(headers)})
+        writer.pipe(res)
+        for (var page in report) {
+          var row = report[page]
+          writer.write([page].concat(headers.map(val)))
+          function val(key) {return row[key]}
+        }
+        writer.end()
+      }
+    }
+
+    function ratingsJson(req, res) {
+      collectRatings(collectDone)
+      function collectDone(err, report) {
+        if (err) return res.send(err.message)
+        res.set({'Content-type': 'application/json'})
+        res.send(report)
+      }
+    }
+
+    function collectRatings(ratingsDone) {
       var report = {}
-
       glob('*', { cwd: argv.db }, allfiles)
-
       function allfiles(err, files) {
         if (err) return done(err)
-        async.map(files||[], eachfile, done) 
+        async.map(files||[], eachfile, allfilesDone)
       }
-
-      function eachfile(file, done) {
+      function allfilesDone(err) {
+        if (err) return ratingsDone(err)
+        ratingsDone(null, report)
+      }
+      function eachfile(file, eachFileDone) {
         jsonfile.readFile(argv.db + '/' + file, {throws:false}, eachpage)
-
         function eachpage(err, page) {
           if (err) return done(err)
           var title = page.title || 'untitled'
@@ -41,19 +73,11 @@
           if(Object.keys(stars).length) {
             report[title] = stars
           }
-          done (null, title)
+          eachFileDone(null, title)
         }
       }
-
-      function done(err) {
-        if (err) return res.send(err.message)
-        res.set({'Content-type': 'application/json'})
-        res.send(report)
-      }
     }
-
   }
-
 
   module.exports = {
     startServer: startServer
